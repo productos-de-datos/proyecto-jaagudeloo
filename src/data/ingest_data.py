dae46dcf5dfa1f791bbac0a11d78cbb520edf75b
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-25 18:49:30 -0500

    Lab1_v3
    Lab1_v3

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,7 +1,10 @@
 """
 Módulo de ingestión de datos.
 -------------------------------------------------------------------------------
-En este módulo se ingresan los datos externos a la capa landing del data lake
+En este módulo se descargan los datos externos y se almacenan en la carpeta landing del data lake
+
+>>> ingest_data()
+
 """
 
 def ingest_data():
@@ -12,9 +15,29 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
-    
+    import os
+    import urllib.request
+
+    anio_inicial = 1995
+    anio_final = 2021
+    rango_anios = list(range(anio_inicial, anio_final + 1, 1))
+
+    for anio in rango_anios:
+        try:
+            archivo = open(f'data_lake/landing/{anio}.xlsx', 'wb')
+            ruta = urllib.request.urlopen(f'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{anio}.xlsx?raw=true')
+            archivo.write(ruta.read())
+            archivo.close()
+        except:
+            archivo.close()
+            os.remove(f'data_lake/landing/{anio}.xlsx')
+
+            archivo = open(f'data_lake/landing/{anio}.xls', 'wb')
+            ruta = urllib.request.urlopen(f'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{anio}.xls?raw=true')
+            archivo.write(ruta.read())
+            archivo.close()
 
 if __name__ == "__main__":
     import doctest
