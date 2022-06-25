9d0b8491fed94064fa606e583f867f14cf48b346
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-25 14:27:49 -0500

    Lab1_v0
    Lab1_v0

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -1,3 +1,18 @@
+"""
+Modulo de creación de directorios
+----------------------------------------
+Este modulo crea el esquema de carpetas que servirán para contener los diferentes datos y resultados
+
+>>> create_data_lake()
+assert os.path.isdir("data_lake/business") is True
+assert os.path.isdir("data_lake/business/reports/figures") is True
+assert os.path.isdir("data_lake/business/features") is True
+assert os.path.isdir("data_lake/business/forecasts") is True
+assert os.path.isdir("data_lake/cleansed") is True
+assert os.path.isdir("data_lake/landing") is True
+assert os.path.isdir("data_lake/raw") is True
+"""
+
 def create_data_lake():
     """Cree el data lake con sus capas.
 
@@ -21,8 +36,19 @@ def create_data_lake():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
+    import os
+
+    os.mkdir('data_lake', exist_ok=True)
+    os.mkdir('data_lake/landing', exist_ok=True)
+    os.mkdir('data_lake/raw', exist_ok=True)
+    os.mkdir('data_lake/cleansed', exist_ok=True)
+    os.mkdir('data_lake/business', exist_ok=True)
+    os.mkdir('data_lake/business/reports', exist_ok=True)
+    os.mkdir('data_lake/business/reports/figures', exist_ok=True)
+    os.mkdir('data_lake/business/features', exist_ok=True)
+    os.mkdir('data_lake/business/forecasts', exist_ok=True)
 
 if __name__ == "__main__":
     import doctest
