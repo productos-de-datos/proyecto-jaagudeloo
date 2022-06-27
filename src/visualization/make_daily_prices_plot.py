12ea72b78d33eda1e1f26eacd8a8db531f3eaedf
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-27 12:47:33 -0500

    Lab1_v17
    Lab1_v17

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -22,9 +22,9 @@ def make_daily_prices_plot():
     import os
     import pandas as pd
 
-    precios_diarios = pd.read_csv('../data/data_lake/cleansed/precios-horarios.csv')
+    precios_diarios = pd.read_csv('data_lake/cleansed/precios-horarios.csv')
     fig_precios_diarios = precios_diarios.plot(x = 'fecha', y = 'precio', xlabel = 'fecha', ylabel = 'precio', kind = 'line', figsize = (16,8), grid = True, title = 'Promedios Diarios').get_figure()
-    fig_precios_diarios.savefig('../data/data_lake/business/reports/figures/daily_prices.png')
+    fig_precios_diarios.savefig('data_lake/business/reports/figures/daily_prices.png')
 
 if __name__ == "__main__":
     import doctest
